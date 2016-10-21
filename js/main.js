7d9b316bc6ba637bc3b51f7397872f4f851a6b07
Jason Lee <jleeisme@gmail.com>
2016-10-21 16:08:07 -0700

    Added a main.js file
    Includes a function that generates a random color on keyup.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// eventually include something silly like an input that serves no purpose except to change the background 
+// color overlay on keyups. Will need to include something for opacity so it doesn't completely cover up the 
+// existing background. 
+
+// function randomColorChange(){
+//   return '#'+(Math.floor(Math.random()*16777216)&0xFFFFFF).toString(16);
+// }
+// $('#temp').on('blur keyup', function() {
+//   $('body').css('background-color', randomColorChange());
+// });
+
