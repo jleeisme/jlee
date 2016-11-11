c2dfd7a2fe1069c9d62f21df520eae3320d76153
Jason Lee <jleeisme@gmail.com>
2016-11-11 13:04:34 -0800

    added an event
    Removes the ghost-font class when clicked outside of that element

--- js/main.js
+++ js/main.js
@@ -1,4 +1,34 @@
-$('.hamburger').on('click', function(){  
-  $('.dropdown').slideToggle(500);
+$(function() {
+
+  $('.hamburger').on('click', function(){  
+    $('.dropdown').slideToggle(500);
+  });
+
+  $('a').on('click', function(){
+    $(this).addClass('ghost-font');
+  });
+
+  $(document).on('click', function(e){
+    if ($(e.target).is('a') === false) {
+      $('a').removeClass('ghost-font');
+    }
+  });
+
 });
 
+
+
+// window.onclick = function(event) {
+//   if (!event.target.matches('a')) {
+
+//     var dropdowns = document.getElementsByClassName("hamburger");
+//     var bottomLinks = document.getElementByClassName("nav-link");
+//     var i;
+//     for (i = 0; i < dropdowns.length || bottomLinks.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('ghost-font')) {
+//         openDropdown.classList.remove('ghost-font');
+//       }
+//     }
+//   }
+// }
