f7d9d5defb04d2f3252054e2c1ce170bb6973f0b
Jason Lee <jleeisme@gmail.com>
2016-11-12 15:22:24 -0800

    Changed the second h1 phrase
    Increased slideToggle to (800)

--- js/main.js
+++ js/main.js
@@ -3,20 +3,19 @@ $(function() {
   // var timer = setTimeout(3000);
 
   setTimeout(function(){
-
     $('.cover-heading').addClass('ghost-font');
     $('.cover-heading-bottom').addClass('font-erase');
   }, 4700);
 
   $('.hamburger').on('click', function(){  
-    $('.dropdown').slideToggle(500);
+    $('.dropdown').slideToggle(800);
   });
 
-  if ($(window).width() > 769){
+  if ($(window).width() > 769){// mobile sites aren't really that nice. Be honest.
     $('a').on('click', function(){
       $(this).addClass('ghost-font');
     });
-    $(document).on('click', function(e){
+    $(document).on('click', function(e){// cancels the ghost if clicked oustide 'a'
       if ($(e.target).is('a') === false) {
         $('a').removeClass('ghost-font');
       }
